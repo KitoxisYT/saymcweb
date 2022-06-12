4ce837e654bef3deb4c2afb8700f69c232de2f6b
KitoxisYT <77733947+KitoxisYT@users.noreply.github.com>
2022-06-12 12:14:06 +0300

    SayMC

---
+++ config.js
@@ -0,0 +1,16 @@
+/*!
+ * Quara Theme by OllieJW (https://olliejw.me)
+ * License - https://olliejw.me/tos
+ */
+
+var server_name = "SayMC";
+var server_ip = "mc.saymc.tk";
+var server_port = 25020;
+var ip_copied = "IP copied!";
+
+var firefly_count = 20;
+
+staff("VainiusSay", "3915b0b1-6a3e-4050-bb66-d2c207d29086", "Savininkas");
+staff("kitoxis", "db07b6ee-d3b8-408e-978c-932969c8a585", "Savininkas");
+
+
